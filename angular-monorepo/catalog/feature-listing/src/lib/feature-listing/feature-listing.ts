dde38ba98a6d1ef75f52d58aea0d07ee32898cbf
TI-IT <go.tiit.ogle@gmail.com>
2025-06-19 23:17:31 +0300

    001_lesson

---
+++ angular-monorepo/catalog/feature-listing/src/lib/feature-listing/feature-listing.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-feature-listing',
+  imports: [CommonModule],
+  templateUrl: './feature-listing.html',
+  styleUrl: './feature-listing.css',
+})
+export class FeatureListing {}
